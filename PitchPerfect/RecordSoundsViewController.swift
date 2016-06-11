4ac6690bd8c1d096e3e00acc351c78ac01445fbe
Martín Montes Pérez <martin@tahiti.local>
2016-06-11 21:26:18 +0200

    Routes added

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RecordSoundsViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var recordingButton: UIButton!
